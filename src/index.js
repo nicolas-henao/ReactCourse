7c562f7d1733a5f0877dd80908aae066b5bfc1db
nicolas-henao <nicohenaor@gmail.com>
2022-11-17 11:47:53 -0500

    useState

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-
-
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
 import Product, { Navbar } from "./Product";
@@ -11,31 +9,26 @@ import { Posts } from "./Posts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const user = [
-  {
-    id: 1,
-    name: "Javier",
-    image: "https://robohash.org/user2"
-  },
-  {
-    id: 2,
-    name: "Bonnteylor",
-    image: "https://robohash.org/kk"
-  },
-    {
-    id: 3,
-    name: "Santi",
-    image: "https://robohash.org/negro"
-  }
-]
+function Counter() {
+
+  const [ counter, setCounter ] = useState(10)
+
+  return (
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Sumar
+      </button>
+    </div>
+  );
+}
 
 root.render(
   <>
-    {user.map((user, i) => {
-      return <div key = { i}>
-        <h1>{user.name}</h1>
-        <img src={user.image} />
-      </div>
-    })}
+    <Counter />
   </>
 );
