e1cc3c5d4ac21d457fdcea57383525926b8a27a8
nicolas-henao <nicohenaor@gmail.com>
2022-11-21 07:30:25 -0500

    useEffect

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
 import Product, { Navbar } from "./Product";
@@ -10,20 +10,27 @@ import { Posts } from "./Posts";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Counter() {
+  const [ mensaje, setMensaje ] = useState('');
+  const [ counter, setCounter ] = useState(0);
 
-  const [ counter, setCounter ] = useState(10)
+  useEffect(() => {
+    console.log('render')
+  }, [ ])
 
   return (
     <div>
-      <h1>Counter: {counter}</h1>
-      <button
-        onClick={() => {
-          setCounter(counter + 1);
-        }}
-      >
-        Sumar
-      </button>
+    <input onChange={e => setMensaje(e.target.value)} />
+    <button onClick={() => {
+      alert("Mensaje: " + mensaje)
+    }} >Save</button>
+    <hr/>
+
+    <h1>Counter: {counter}</h1>
+    <button onClick={() => setCounter(counter + 1)}>Increment</button>
     </div>
+
+    
+
   );
 }
 
